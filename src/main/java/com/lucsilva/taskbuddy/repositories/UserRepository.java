87ca097576b68fea84e87d008459635c1fb1fd2f
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-09 17:00:38 -0300

    refactor: getUserAccountByEmail now returns a Optimal object

--- src/main/java/com/lucsilva/taskbuddy/repositories/UserRepository.java
+++ src/main/java/com/lucsilva/taskbuddy/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import com.lucsilva.taskbuddy.entities.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserAccount, Integer> {
     @Query("SELECT User FROM UserAccount User WHERE User.email = ?1")
-    public UserAccount getUserAccountByEmail(String email);
+    public Optional<UserAccount> getUserAccountByEmail(String email);
 }
