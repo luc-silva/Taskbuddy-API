203d9905c98c58ed20d4a9f9dfaf4febab73fa81
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-12 17:42:56 -0300

    feat: improves project creation and delete methods

--- src/main/java/com/lucsilva/taskbuddy/services/ProjectService.java
+++ src/main/java/com/lucsilva/taskbuddy/services/ProjectService.java
@@ -1,14 +1,19 @@
 package com.lucsilva.taskbuddy.services;
 
 import com.lucsilva.taskbuddy.entities.Project;
+import com.lucsilva.taskbuddy.entities.ProjectTask;
 import com.lucsilva.taskbuddy.entities.UserAccount;
+import com.lucsilva.taskbuddy.exceptions.NotFound;
 import com.lucsilva.taskbuddy.repositories.ProjectRepository;
 import com.lucsilva.taskbuddy.repositories.ProjectTaskRepository;
 import com.lucsilva.taskbuddy.repositories.UserRepository;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ProjectService {
@@ -25,8 +30,21 @@ public class ProjectService {
         if (project.getProjectTasks().isEmpty()) {
             throw new IllegalArgumentException("Project must have at least one task.");
         }
+        if(project.getUser() == null){
+            throw new IllegalArgumentException("Project must have an owner.");
+        }
+
+        Optional<UserAccount> userAccount = userRepository.findById(project.getUser().getId());
+        if(userAccount.isEmpty()){
+            throw new NotFound("User not found.");
+        }
+        UserAccount projectOwner = userAccount.get();
+        projectOwner.getProjects().add(project);
+
         projectRepository.save(project);
         projectTaskRepository.saveAll(project.getProjectTasks());
+        userRepository.save(projectOwner);
+
 
     }
 
@@ -40,8 +58,15 @@ public class ProjectService {
         Optional<Project> project = projectRepository.findById(id);
         if (project.isPresent()) {
             Project projectFound = project.get();
-            Optional<UserAccount> user = userRepository.findById(projectFound.getUser().getId());
-            user.get().getProjects().remove(project.get());
+            Optional<UserAccount> projectOwner = userRepository.findById(projectFound.getUser().getId());
+            if(projectOwner.isEmpty()){
+                throw new NotFound();
+            }
+            UserAccount user = projectOwner.get();
+            user.getProjects().remove(project.get());
+
+            Set<ProjectTask> projectTask = projectTaskRepository.getProjectTaskByProjectId(projectFound.getId());
+            projectTaskRepository.deleteAll(projectTask);
             projectRepository.delete(project.get());
         }
 
