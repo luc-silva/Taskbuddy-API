ece2270f7963c34f6443ef851d688ad4607a5566
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-13 18:04:42 -0300

    feat: improve request error handling and new field to error response

--- src/main/java/com/lucsilva/taskbuddy/exceptions/ErrorResponseBody.java
+++ src/main/java/com/lucsilva/taskbuddy/exceptions/ErrorResponseBody.java
@@ -7,17 +7,20 @@ import java.io.Serializable;
 public class ErrorResponseBody implements Serializable {
     String message;
     HttpStatus status;
+    Integer status_code;
 
     public ErrorResponseBody() {
     }
 
     public ErrorResponseBody(String message, HttpStatus status) {
         this.message = message;
+        this.status_code = status.value();
         this.status = status;
     }
 
     public ErrorResponseBody(String message, int status) {
         this.message = message;
+        this.status_code = status;
         this.status = HttpStatus.valueOf(status);
     }
 
@@ -37,11 +40,20 @@ public class ErrorResponseBody implements Serializable {
         this.status = status;
     }
 
+    public Integer getStatus_code() {
+        return status_code;
+    }
+
+    public void setStatus_code(Integer status_code) {
+        this.status_code = status_code;
+    }
+
     @Override
     public String toString() {
         return "ErrorResponseBody{" +
                 "message='" + message + '\'' +
                 ", status=" + status +
+                ", status_code=" + status_code +
                 '}';
     }
 }
--- src/main/java/com/lucsilva/taskbuddy/exceptions/ResponseExceptionHandler.java
+++ src/main/java/com/lucsilva/taskbuddy/exceptions/ResponseExceptionHandler.java
@@ -12,9 +12,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = NotFound.class)
-    public ResponseEntity<?> handleException(NotFound e) {
+    public ResponseEntity<?> handleNotFoundException(NotFound e) {
         ErrorResponseBody erb = new ErrorResponseBody(e.getMessage(), HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(erb, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<?> handleInvalidData(IllegalArgumentException e) {
+        ErrorResponseBody erb = new ErrorResponseBody(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(erb, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = NotAuthenticated.class)
+    public ResponseEntity<?> handleUnauthorizedAccess(NotAuthenticated e) {
+        ErrorResponseBody erb = new ErrorResponseBody(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(erb, HttpStatus.UNAUTHORIZED);
     }
 
 }
