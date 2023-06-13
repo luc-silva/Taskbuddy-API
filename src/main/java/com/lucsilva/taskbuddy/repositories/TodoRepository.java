d8b09d6bbeb688e90e17a263707d9bb0f8793eb8
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-13 16:58:03 -0300

    feat: add listUserTodos and updateTodoById methods

--- src/main/java/com/lucsilva/taskbuddy/repositories/TodoRepository.java
+++ src/main/java/com/lucsilva/taskbuddy/repositories/TodoRepository.java
@@ -1,7 +1,19 @@
 package com.lucsilva.taskbuddy.repositories;
 
 import com.lucsilva.taskbuddy.entities.Todo;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
 
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
+    @Query("SELECT Todo FROM Todo WHERE user = ?1")
+    public Set<Todo> listUSerTodos(Integer userId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Todo SET concluded = ?2 WHERE id = ?1")
+    public void updateTodoById(Integer id, Boolean todoStatus);
 }
