1f6c23861f249e9d1d1e3c214e985c718baff989
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-22 17:46:25 -0300

    fix; add missing CrossOrigin annotation on controllers

--- src/main/java/com/lucsilva/taskbuddy/resources/ProjectResource.java
+++ src/main/java/com/lucsilva/taskbuddy/resources/ProjectResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/project")
 public class ProjectResource {
--- src/main/java/com/lucsilva/taskbuddy/resources/UserResource.java
+++ src/main/java/com/lucsilva/taskbuddy/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/user")
 public class UserResource {
